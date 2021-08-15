69f078de55b85981751dff7a92b0c914bdf21c49
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-15 19:00:08 +0530

    Add files via upload

---
+++ displ;aysize.c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int r=25;
+    printf("the size of int is: %2d\n",sizeof(r));
+     printf("the size of long is: %2d\n",sizeof(long));
+      printf("the size of double 2d\n",sizeof(double));
+       printf("the size of short is: %2d\n",sizeof(short));
+        printf("the size of char is: %2d\n",sizeof(char));
+         printf("the size of float is: %2d\n",sizeof(float));
+          printf("the size of double is: %2d\n",sizeof(long double));
+
+
+
+    return 0;
+}
