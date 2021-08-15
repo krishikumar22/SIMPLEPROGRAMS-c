c844ee10106f8e514d4330ef1263927e0bd116ce
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-15 19:00:53 +0530

    Add files via upload

---
+++ power.c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int main()
+{
+
+    int n,a,b,c;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    a=pow(n,1);
+         b=pow(n,2);
+
+             c=pow(n,3);
+
+    printf("the value is %d\n%d\n%d\n",a,b,c);
+    return 0;
+}
