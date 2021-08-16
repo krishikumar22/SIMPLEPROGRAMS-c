c2f22e52acc3e6ac7b51aff91488b9973b2a027a
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-16 18:56:38 +0530

    Add files via upload

---
+++ positive.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int a;
+printf("enter the value of a:");
+scanf("%d",&a);
+if(a<0)
+    printf("the number is negative");
+else
+    printf("the number is positive");
+
+    return 0;
+}
